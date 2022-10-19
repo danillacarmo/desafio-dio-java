68a44685ba9d9d73afa188f2d5cfc030f62d0a6d
Danilla Carmo <danillacarolina@yahoo.com.br>
2022-10-19 10:33:44 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,9 +22,39 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devDanilla = new Dev();
+        devDanilla.setNome("Danilla");
+        devDanilla.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devDanilla.getConteudosInscritos());
+        devDanilla.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos" + devDanilla.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devDanilla.getConteudosConcluidos());
+        System.out.println("XP" + devDanilla.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devJoao.getConteudosConcluidos());
+        System.out.println("XP" + devJoao.calcularTotalXp());
+
     }
 
 }
